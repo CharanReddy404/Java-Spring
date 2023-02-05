f532ffb3c7bb085f1f4c6abc746895c8acc46331
charanreddy404 <charanlucky6143@gmail.com>
2023-02-05 06:27:52 +0530

    using ModelAndView

--- CoursesWebApp/src/main/WebApp/webpages/course.jsp
+++ CoursesWebApp/src/main/WebApp/webpages/course.jsp
@@ -7,11 +7,6 @@
 <title>Courses</title>
 </head>
 <body>
-Top Selling Courses are:<br><br>
-1. DevOps<br>
-2. Java<br>
-3. JavScript<br>
-4. React<br>
-5. NodeJs<br>
+Welcome to SpringBoot.... // ${cName}
 </body>
 </html>
--- CoursesWebApp/src/main/java/com/example/demo/CoursesController.java
+++ CoursesWebApp/src/main/java/com/example/demo/CoursesController.java
@@ -2,13 +2,39 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class CoursesController {
 	
 	@RequestMapping("/courses")
-	public String courses() {
-		System.out.println("Welcome to Spring Boot..");
-		return "course";
+	public ModelAndView courses(@RequestParam("cname")
+			String coursename) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("cName", coursename);
+		mv.setViewName("course");
+		return mv;
 	}
+	
+//	@RequestMapping("/courses")
+//	public String courses(@RequestParam("cname")
+//			String coursename, HttpSession session) {
+//		
+//		session.setAttribute("cName", coursename);
+//		return "course";
+//	}
+	
+//	@RequestMapping("/courses")
+//	public String courses(HttpServletRequest req) {
+//		HttpSession session = req.getSession();
+//		System.out.println("Welcome to Spring Boot..");
+//		String cName = req.getParameter("cname");
+//		System.out.println("The Course name is: "+cName);
+//		session.setAttribute("cName", cName);
+//		return "course";
+//	}
 }
