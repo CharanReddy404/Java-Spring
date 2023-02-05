3e70896ac846c1bc1e33e17561f2339c59d530c5
charanreddy404 <charanlucky6143@gmail.com>
2023-02-05 22:54:24 +0530

    spring CRED operation

--- SubmissionForm/src/main/java/com/example/demo/FormController.java
+++ SubmissionForm/src/main/java/com/example/demo/FormController.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -69,6 +72,25 @@ public class FormController {
 	@ResponseBody
 	public Optional<Customers> getCustomers2(@PathVariable("cid") int cid) {
 		return repo.findById(cid);
+	}
+	
+	@PostMapping("/customers")
+	public Customers getCustomers3(@RequestBody Customers customers) {
+		repo.save(customers);
+		return customers;
+	}
+	
+	@DeleteMapping("/customers/{cid}")
+	public Customers getCustomers4(@PathVariable("cid") int cid) {
+		Customers cust = repo.getOne(cid);
+		repo.delete(cust);
+		return cust;
+	}
+	
+	@PutMapping(path = "/customers", consumes = {"application/json"})
+	public Customers getCustomers5(@RequestBody Customers customers) {
+		repo.save(customers);
+		return customers;
 	}
 	
 }
