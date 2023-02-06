2e10035be9570d595e91467a3df90db64afa5b5e
charanreddy404 <charanlucky6143@gmail.com>
2023-02-06 07:42:55 +0530

    spring rest completed

--- DataRestDemo/src/main/java/com/example/demo/Customers.java
+++ DataRestDemo/src/main/java/com/example/demo/Customers.java
@@ -1,5 +1,45 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customers {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int cid;
+	private String cname;
+	private String coursename;
+	
+	public int getCid() {
+		return cid;
+	}
+	
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	
+	public String getCname() {
+		return cname;
+	}
+	
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
+	public String getCoursename() {
+		return coursename;
+	}
+	
+	public void setCoursename(String coursename) {
+		this.coursename = coursename;
+	}
+
+	@Override
+	public String toString() {
+		return "Customers [cid=" + cid + ", cname=" + cname + ", coursename=" + coursename + "]";
+	}
 
 }
